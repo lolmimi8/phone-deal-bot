6bcdfcb0e5cf9e4caf7e5ae4b2e96fa04d2cedb9
lolmimi8 <gejtarnowski0@gmail.com>
2026-03-17 19:21:31 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -4,8 +4,9 @@ import threading
 class PingHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"PhoneDealBot is alive!")
+        self.wfile.write(b"OK")
     def log_message(self, *args):
         pass
 
@@ -13,4 +14,4 @@ def keep_alive():
     server = HTTPServer(("0.0.0.0", 8080), PingHandler)
     t = threading.Thread(target=server.serve_forever, daemon=True)
     t.start()
-    print("[KeepAlive] Serwer HTTP nasluchuje na :8080")
+    print("[KeepAlive] Serwer HTTP dziala na porcie 8080")
